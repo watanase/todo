df0b94c32da3ec247057cb8a61364b984b341be1
watanase <veltread@gmail.som>
2020-05-23 22:14:43 +0900

    .

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var app = new Vue({
   el: '#app',
   data: {
-    currentTab: ""
+    left: 0,
+    right: 0,
+    isComputedCalled: false
   },
   methods: {
-    toggle: function (tab) {
-      this.currentTab = tab
+    // この関数が呼び出されたタイミングがわかるように
+    // メッセージを表示させる
+    setMessage: function () {
+      // メッセージを表示
+      this.isComputedCalled = true
+      const that = this
+      setTimeout(function () {
+        that.isComputedCalled = false
+      }, 1000) // 1秒後にメッセージが消えます
+    }
+  },
+  computed: {
+    total: function () {
+      this.setMessage()
+
+      return this.left + this.right
     }
   }
 })
